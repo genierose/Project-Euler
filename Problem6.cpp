b0f6f7694414ebf791b2ef1601f589aee4924353
HyeokjinSeo <31676324+mirein@users.noreply.github.com>
2018-08-20 17:21:42 +0900

    Add files via upload

---
+++ Problem6.cpp
@@ -0,0 +1,25 @@
+/* 시그마의 성질에 의하여 S(X)^2 - S(X^2)은
+(n*n+1)/2) ^2 - n(n+1)(2n+1)/6
+정리하면 ((n - 1) * n * (n + 1) * (3*n + 2))/12
+*/
+
+/*#include<stdio.h>
+#include<iostream>
+#include<algorithm>
+using namespace std;
+
+int solve(int n) {
+	
+	int result = 0;
+	result =((n - 1) * n * (n + 1) * (3*n + 2))/12;
+	return result;
+}
+
+
+int main() {
+	
+	cout << solve(100) << endl;
+
+}
+*/
+
